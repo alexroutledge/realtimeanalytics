9dd430718aa1cf9a9980a9799c939e1b32d14c17
alexroutledge <alex.routledge@gmail.com>
2013-08-14 23:22:58 +0100

    Create analytics.reviews.js

---
+++ js/analytics.reviews.js
@@ -0,0 +1,127 @@
+$(function(){
+
+   Reviews = {};
+   Reviews.Current = {};
+   Reviews.UserCount = {};
+   Reviews.DisplayItem = {};
+   Reviews.Activity = {};
+   Reviews.View = Default.View;
+   Reviews.Model = Default.Model;
+   Reviews.Collection = Backbone.Firebase.Collection.extend({
+    model: Default.Model,
+    firebase: new Firebase("https://alexroutledge.firebaseio.com/reviews")
+   });
+   Reviews.Activity.View = Default.View.extend({
+     template: templates.reviews.activity
+   });
+   Reviews.DisplayItem.View = Default.View.extend({
+     template: templates.reviews.DisplayItem
+   });
+   Reviews.Current.View = Default.View.extend({
+     template: templates.reviews.current
+   });
+   Reviews.UserCount.View = Default.View.extend({
+     template: templates.reviews.userCount
+   });
+
+   var AppView = Backbone.View.extend({
+
+    el: $("#pane4"),
+
+    events: {
+      "update [data-trigger]":  "update",
+      "click [data-reviews-item-remove]" : "clear",
+      "showmap [data-trigger]" : "showMap",
+    },
+
+    initialize: function() {
+      this.listenTo(new Reviews.Collection, 'add', this.addOne);
+      this.listenTo(new Reviews.Collection, 'reset', this.addAll)
+      this.listenTo(new Reviews.Collection, 'all', this.render);
+    },
+
+    showMap: function(a) {
+      var a = 'Reviews';
+      if (!$('[data-reviews-navigation]').is('.active')) {
+        return;
+      }
+      var temp =[];
+      for (i in (new window[a].Collection).models) {
+        temp.push((new window[a].Collection).models[i].attributes.location);
+      }
+      var map = new google.maps.Map(document.getElementById('map_canvas'), {
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+      var geocoder = new google.maps.Geocoder();
+      function createMarkers() {
+        for (var i = 0; i < temp.length; ++i) {
+          (function(address) {
+            geocoder.geocode({
+                'address': address
+            }, function(results) {
+                if (!results) {
+                  return;
+                }
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location,
+                    title: address
+                });
+                map.setCenter(marker.getPosition());
+                map.panTo(marker.getPosition());
+
+            });
+          })(temp[i]);
+        }
+      };
+      createMarkers();
+    },
+
+    render: function(type) {
+      var view = new Reviews.UserCount.View({
+        el: $('[data-reviews-count]')[0],
+        model: {count: (new Reviews.Collection).length}
+      });
+      view.render();
+
+      var view = new Reviews.Current.View({
+        el: $('[data-reviews-current]')[0],
+        model: {count: (new Reviews.Collection).length}
+      });
+      view.render();
+
+      var view = new Reviews.DisplayItem.View({
+        el: $('[data-reviews-list]')[0],
+        model: (new Reviews.Collection)
+      });
+      view.render();
+      this.showMap('Reviews');
+    },
+
+    addOne: function(todo) {
+      var view = new Reviews.Activity.View({
+        el: $('[data-reviews-activity]')[0],
+        model: (new Reviews.Collection).last()
+      });
+      view.render();
+    },
+
+    addAll: function() {
+      (new Reviews.Collection).each(this.addOne, this);
+    },
+   
+    clear: function(e) {
+      var id = $(e.currentTarget).data('id');
+      (new Reviews.Collection).remove((new Reviews.Collection).get(id));
+    },
+
+    update: function(e) {
+      (new Reviews.Collection).add({location: window.pcaLocation});
+    }
+
+  });
+
+  var App = new AppView;
+
+});
